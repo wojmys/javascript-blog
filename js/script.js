d448d814888178810e6522510796fa3e58df9eb7
Wojciech Mysliwiec <wojmys@gmail.com>
2023-11-26 11:35:41 +0100

    add const targetArticle

--- js/script.js
+++ js/script.js
@@ -35,6 +35,9 @@ function titleClickHandler(event) {
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
 }
 
