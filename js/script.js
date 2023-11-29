bfc0f0cd6cbb6a3558fa5cbc4a846dc5c7d1e796
Wojciech Mysliwiec <wojmys@gmail.com>
2023-11-29 11:34:31 +0100

    remove contents of titleList

--- js/script.js
+++ js/script.js
@@ -49,3 +49,29 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+const titleList=document.querySelector(optTitleListSelector).innerHTML = ''
+
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
