f3151db03a2d75e41b4adb222d6429aa944c48ab
Wojciech Mysliwiec <wojmys@gmail.com>
2023-12-02 13:37:36 +0100

    add tags

--- js/script.js
+++ js/script.js
@@ -91,3 +91,31 @@ function generateTitleLinks(){
 
 generateTitleLinks();
 
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
+
